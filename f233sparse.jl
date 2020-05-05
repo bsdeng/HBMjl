9622556cd5f062e07dd17d311c3f7c09a5a89a71
bsdeng <61472308+bsdeng@users.noreply.github.com>
2020-05-06 00:48:56 +0800

    Update f233sparse.jl

--- f233sparse.jl
+++ f233sparse.jl
@@ -1,22 +1,29 @@
-# Julia program to illustrate
-# the use of For loop
+nT = 8
+nh = 1
+n = 2*nh+1
 
-print("List Iteration\n")
-l = ["geeks", "for", "geeks"]
-for i in l
-    println(i)
+Ga = zeros(nT, n)
+x = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in 1:1:nT
+    x=[i]
 end
 
-# Iterating over a tuple (immutable)
-print("\nTuple Iteration\n")
-t = ("geeks", "for", "geeks")
-for i in t
-    println(i)
+y = [1]
+for i in x
+    Ga[i,1] = 1
+    for j in y
+        Ga[i,2*j] = cos(j*i*2*pi/nT)
+        Ga[i,2*j+1] = sin(j*i*2*pi/nT)
+    end
 end
 
-# Iterating over a String
-print("\nString Iteration\n")
-s = "Geeks"
-for i in s
-    println(i)
-end 
+Gp = zeros(n, nT)
+for i in x
+    Gp[1,i] = 1/nT
+    for j in y
+        Gp[2*j,i] = (1/nT)*2*cos(j*i*2*pi/nT)
+        Gp[2*j+1,i] = (1/nT)*2*sin(j*i*2*pi/nT)
+    end
+end
+print(Gp)
